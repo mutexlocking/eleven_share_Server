416383b3a8021247e71be5e3b6828bca632bea91
minaver <psmjoshua@gmail.com>
2022-11-10 21:39:07 +0900

    feat(Scheduler): Room,RoomMember가 DB에 하나도 없을시 Validation

--- src/main/java/com/konkuk/eleveneleven/src/room_member/service/RoomMemberService.java
+++ src/main/java/com/konkuk/eleveneleven/src/room_member/service/RoomMemberService.java
@@ -1,6 +1,8 @@
 package com.konkuk.eleveneleven.src.room_member.service;
 
 import com.konkuk.eleveneleven.common.enums.Gender;
+import com.konkuk.eleveneleven.config.BaseException;
+import com.konkuk.eleveneleven.config.BaseResponseStatus;
 import com.konkuk.eleveneleven.src.member.Member;
 import com.konkuk.eleveneleven.src.member.repository.MemberRepository;
 import com.konkuk.eleveneleven.src.room_member.repository.RoomMemberRepository;
@@ -10,5 +12,14 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class RoomMemberService {
+
+    private final RoomMemberRepository roomMemberRepository;
+
+    /** DB상 RoomMember가 비어있는지 확인 */
+    public void checkIsRoomMemberEmpty(){
+        if (roomMemberRepository.findAll().size() == 0) {
+            throw new BaseException(BaseResponseStatus.ROOM_MEMBER_EMPTY_IN_DB);
+        }
+    }
 
 }
--- src/main/java/com/konkuk/eleveneleven/src/scheduler/service/SchedulerService.java
+++ src/main/java/com/konkuk/eleveneleven/src/scheduler/service/SchedulerService.java
@@ -1,24 +1,27 @@
-package com.konkuk.eleveneleven.src.room.scheduler;
+package com.konkuk.eleveneleven.src.scheduler.service;
+
 
 import com.konkuk.eleveneleven.src.matched_room.service.MatchedRoomService;
 import com.konkuk.eleveneleven.src.room.service.RoomMatchingService;
 import com.konkuk.eleveneleven.src.room.service.RoomService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
 
-@Controller
+@Service
+@EnableScheduling
 @RequiredArgsConstructor
-public class SchedulerController {
+public class SchedulerService {
 
     private final RoomMatchingService roomMatchingService;
     private final RoomService roomService;
     private final MatchedRoomService matchedRoomService;
 
     /** 매일 23시에 Scheduler를 동작하여 matching 진행 */
-    @PostMapping("/schedule/test")
     @Transactional
+//    @Scheduled(cron = "0 11 23 * * ?")
+//    @Scheduled(cron = "0 32 19 * * ?")
     public void matchRoom() {
         roomMatchingService.randMatchRoom();
         matchedRoomService.migrateRoomToMatchedRoom();
